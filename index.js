596003b0a9276c36787bd827cba82d508f40dcaa
vangho45-prog <vangho45@uwosh.edu>
2026-05-08 14:43:52 +0000

    Added a line with my initials

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => res.send('hello world'));
+app.get('/hv', (req, res) => res.send('hello from hv'));
 
 app.listen(3000, () => { console.log('Listening on port 3000'); });
